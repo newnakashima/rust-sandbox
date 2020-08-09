ed2789a8204e209cf5c429c399b6ede9fd8cc50b
newnakashima <newnakashima2014@gmail.com>
2020-08-09 16:50:16 +0900

    自分で考案したsqrtの実装ダメダメだった

--- sqrt/src/main.rs
+++ sqrt/src/main.rs
@@ -4,30 +4,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let num: f64 = args[1].parse().unwrap_or(0.0);
 
-    let mut count = 100;
-    let mut larger_number = num;
-    let mut smaller_number = 0.0;
-    let mut root = 0.0;
+    println!("mysqrt = {}", mysqrt(num));
+}
 
-    while count != 0 {
-        let avg: f64 = (larger_number + smaller_number) / 2.0;
-        let powered: f64 = avg * avg;
-        if powered > num {
-            larger_number = avg;
-            root = avg;
-        } else if powered < num {
-            smaller_number = (smaller_number + avg) / 2.0;
-            root = avg;
+fn mysqrt(num: f64) -> f64 {
+    let mut s: f64;
+    let mut last: f64 = 0.0;
+
+    if num > 0.0 {
+        if num > 1.0 {
+            s = num;
         } else {
-            root = avg;
+            s = 1.0;
         }
-        count = count - 1;
+
+        loop {
+            last = s;
+            s = (num / s + s) / 2.0;
+            if s >= last {
+                break;
+            }
+        }
     }
 
-    let rounded = root.round();
-    if rounded * rounded == num {
-        root = rounded;
-    }
-
-    println!("root = {}", root);
+    return last;
 }
