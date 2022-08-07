822907d8565a1ec88a4e8f9a83250369c4357a98
newnakashima <newnakashima2014@gmail.com>
2022-08-07 14:09:32 +0900

    サンプルコードを試す

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -9,4 +9,38 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
+
+    let s = String::from("hello");
+
+    let s = takes_ownership(s);
+
+    println!("{}", s);
+
+    let x = 5;
+
+    makes_copy(x);
+
+    println!("{}", x);
+
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn takes_ownership(some_string: String) -> String {
+    println!("{}", some_string);
+
+    some_string
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
