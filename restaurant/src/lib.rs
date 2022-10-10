a49db52569c6f056257958a2831470ab9539386e
newnakashima <newnakashima2014@gmail.com>
2022-10-10 15:13:57 +0900

    modify restaurant

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 fn deliver_order() {}
 
@@ -42,6 +30,14 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+}
+
+mod customer {
+    use crate::front_of_house::hosting;
+
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist();
+    }
 }
 
 pub fn eat_at_restaurant() {
