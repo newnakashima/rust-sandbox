3c6de492a66a5c99866549a6c28b2543ccc72286
newnakashima <newnakashima2014@gmail.com>
2022-10-19 23:24:46 +0900

    struct使用

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,34 @@
 use std::io;
 use std::cmp::Ordering;
-use std::process::{exit};
+use std::num::ParseIntError;
 use rand::Rng;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn validate(input: &str) -> Result<i32, ParseIntError> {
+        input.trim().parse()
+    }
+
+    pub fn new(value: &str) -> Guess {
+        let value = match Guess::validate(value) {
+            Ok(v) => v,
+            Err(_) => panic!("{} is not a number. Please input a number", value.trim()),
+        };
+
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Guess the number!");
@@ -17,17 +44,19 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        guess = guess.trim().to_string();
-        let guess_num: u32 = match guess.parse() {
-            Ok(num) => num,
-            Err(_) => {
-                if guess == "quit" {
-                    exit(0);
-                }
-                println!("{guess} is not a number.");
-                continue;
-            }
-        };
+        // guess = guess.trim().to_string();
+        // let guess_num: u32 = match guess.parse() {
+        //     Ok(num) => num,
+        //     Err(_) => {
+        //         if guess == "quit" {
+        //             exit(0);
+        //         }
+        //         println!("{guess} is not a number.");
+        //         continue;
+        //     }
+        // };
+        let guess = Guess::new(&guess);
+        let guess_num = guess.value;
 
         println!("You guessed: {guess_num}");
 
