5d2de5a543f8fb7aa8806019e6f08cf74982fcbf
newnakashima <newnakashima2014@gmail.com>
2022-11-03 00:33:28 +0900

    c-13-1途中まで

--- c-13-1/src/main.rs
+++ c-13-1/src/main.rs
@@ -54,4 +54,24 @@ fn main() {
     let example_closure = |x| x;
     let s = example_closure(String::from("hello"));
     // let n = example_closure(5);
+
+
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let mut borrows_mutably = || list.push(7);
+    // println!("{:?}", list);
+
+    borrows_mutably();
+    println!("After calling closure: {:?}", list);
 }
